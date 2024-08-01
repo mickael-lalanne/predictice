mickael-lalanne/predictice

commit 8755e5a7c0023b396ee890b6d8299abffd5639f9

Move home page to root

components/AppHeader.vue
@@ -56,7 +56,7 @@ type Link = {
 const links: Link[] = [
     {
         label: 'Home',
-        to: '/home',
+        to: '/',
     },
     {
         label: 'PrediReflex',

pages/home/index.vue
@@ -1,34 +1,0 @@
-<template>
-    <div class="flex flex-col place-items-center">
-        <UCard
-            v-for="post in data"
-            :key="post.id"
-            class="mb-5 cursor-pointer hover:outline hover:outline-1 hover:outline-primary dark:hover:outline-primary w-1/2"
-            @click="onPostClick(post)"
-        >
-            <template #header>
-                <div class="flex justify-between">
-                    <span class="text-xl">{{ post.title }}</span>
-                    <span class="text-primary">{{ post.id }}</span>
-                </div>
-            </template>
-
-            <span class="text-base">
-                {{ post.body }}
-            </span>
-        </UCard>
-    </div>
-</template>
-
-<script setup lang="ts">
-useSeoMeta({
-    title: 'Home',
-    description: 'A list of the best posts.',
-});
-
-const { data } = await useFetch<Post[]>('/api/posts');
-
-const onPostClick = async (post: Post) => {
-    await navigateTo(`/home/${post.id}`);
-};
-</script>

pages/index.vue
@@ -1,10 +1,34 @@
 <template>
-    <div>Welcome in the Predictice challenge by Mickaël !</div>
+    <div class="flex flex-col place-items-center">
+        <UCard
+            v-for="post in data"
+            :key="post.id"
+            class="mb-5 cursor-pointer hover:outline hover:outline-1 hover:outline-primary dark:hover:outline-primary w-1/2"
+            @click="onPostClick(post)"
+        >
+            <template #header>
+                <div class="flex justify-between">
+                    <span class="text-xl">{{ post.title }}</span>
+                    <span class="text-primary">{{ post.id }}</span>
+                </div>
+            </template>
+
+            <span class="text-base">
+                {{ post.body }}
+            </span>
+        </UCard>
+    </div>
 </template>
 
 <script setup lang="ts">
 useSeoMeta({
-    title: 'Predictice Challenge',
-    description: 'By Mickaël Lalanne.',
+    title: 'Home',
+    description: 'A list of the best posts.',
 });
+
+const { data } = await useFetch<Post[]>('/api/posts');
+
+const onPostClick = async (post: Post) => {
+    await navigateTo(`/home/${post.id}`);
+};
 </script>

commit abd8edce36426930cf3ad86decfe3c394da33d8d

AppHeader : activate home link on detailed post page
Regression introduced in 8755e5a7c0023b396ee890b6d8299abffd5639f9

components/AppHeader.vue
@@ -71,6 +71,10 @@ const links: Link[] = [
 const route = useRoute();
 
 const isActiveLink = (path: string) => {
-    return route.path === path || route.path.startsWith(`${path}/`);
+    return (
+        route.path === path
+        || route.path.startsWith(`${path}/`)
+        || (path === '/' && route.path.startsWith('/home'))
+    );
 };
 </script>